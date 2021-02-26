72a112d77229f33c04cabd5d8d977785c0840ef2
cacingsuper <cacingzuper@gmail.com>
2021-02-26 19:29:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = exporess()
+const app = express()
 
 app.get('/' , (req,res) => {
   res.send("Hello from Heroku")
