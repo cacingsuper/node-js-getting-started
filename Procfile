a4960e5c0b2cf4b50743ba87941ae963e9210d46
cacingsuper <cacingzuper@gmail.com>
2021-02-26 23:44:01 +0700

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start && npm migrate
+web: npm start
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from '../server/routes';
+import http from 'http'
+
 const PORT = process.env.PORT || 5000
 const app = express();
+const server = http.createServer(app);
+const hostname = '127.0.0.1';
 
 routes(app)
 
@@ -21,8 +25,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
   
 
-const startServer = async () => {
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-}
 
-startServer()
+server.listen(PORT,hostname, () => console.log(`Server running at http://${hostname}:${PORT}/`))
+
+
+
